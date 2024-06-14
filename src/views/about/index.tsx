71970f3cf89339179de1e50cc7f2732c48d4301f
Nguyen Hoang Anh Tuan <atuan.uit@gmail.com>
2024-06-14 21:10:43 +0700

    feat(docs): Update About Ngo

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -3,8 +3,49 @@ import React from "react";
 const Index = () => {
   return (
     <div className="container mx-auto flex flex-col min-h-[64vh]">
-      <h1>About</h1>
-      <p>This is the about page</p>
+      <h1 className="text-center font-bold text-xl">Đôi dòng về Ngõ</h1>
+      <article className="md:w-1/2 p-4 md:border md:border-black mt-4 mb-6 mx-auto md:rounded-lg space-y-2.5">
+        <p className="font-semibold">Ngõ chào bạn!</p>
+        <p>
+          Cảm ơn bạn đã ghé thăm website của Ngõ và cho Ngõ có cơ hội được tâm
+          sự đôi chút về mình.
+        </p>
+        <p>
+          Chúng mình tình cờ biết đến Gốm Nhật trong một lần đi lạc, thật sự là
+          lúc ấy chúng mình đang lạc về cả không gian, thời gian và cả ... tâm
+          trí. Hôm ấy tụi mình đi rong rủi trên các con ngõ ở Sài Gòn trong giờ
+          tan tầm với ánh sáng vàng nhạt của chiều tà len lỏi qua từng góc vắng,
+          tụi mình hẫng một nhịp trước cửa hàng nhỏ không có biển hiệu và thổi
+          vào tai là giai điệu của bản nhạc &ldquo;xưa&rdquo; mang đậm chất hoài
+          niệm ...
+        </p>
+        <p>
+          Chào mình là một Cô lớn tuổi đeo chiếc kính gọng tròn trong suốt, tụi
+          mình cũng đắn đo về việc sẽ vào cửa hàng do lúc ấy thật sự chỉ muốn
+          tìm đường để về nhà, nhưng do không gian ấy và cái chào thân thiện của
+          Cô đã làm tụi mình quên đi những lắng lo trong đầu.
+        </p>
+        <p>
+          Vào tiệm, những kệ gỗ được xếp đầy bởi những chiếc chén, ly, tách, tô
+          ... được làm bằng Gốm. Cô hỏi tụi mình từ đâu tới, &ldquo;Tụi con đang
+          đi lạc Cô ạ!&rdquo; tôi đáp lại cô một cách nhanh nhẹn trong trạng
+          thái không chút nghĩ suy, như cách những người bạn đồng trang lứa chơi
+          thân với nhau hay buôn chuyện cạnh gánh hàng rong.
+        </p>
+        <p>
+          Cô cho tụi mình sạc nhờ điện thoại cho đến khi đủ để tra cứu đường về
+          nhà, trong thời gian đó, tụi mình có cơ hội được nghe Cô kể về chuyện
+          Gốm và cả những lần Cô &ldquo;lạc&rdquo; giống tụi mình trong giai
+          đoạn trưởng thành này. Có lẽ vì Cô đã truyền cho Ngõ một tình yêu đặc
+          biệt với Gốm nên tụi mình cứ ấp ủ và nuôi lớn Ngõ Gốm như hôm nay. Tuy
+          hiện tại, Ngõ Gốm chỉ là cửa hàng nhỏ tọa lạc tại 4/2, đường số 25,
+          phường Long Thạnh Mỹ, thành phố Thủ Đức. Nhưng tụi mình tin rằng, Ngõ
+          Gốm rồi sẽ lớn và trưởng thành thông qua những lần bạn vun đắp bằng
+          cách ghé thăm Ngõ và cho Ngõ cơ hội được vỗ về trạng thái trong tâm
+          hồn bạn!
+        </p>
+        <p className="text-right italic font-semibold">Ngõ Gốm</p>
+      </article>
 
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d354.24688489257414!2d106.83039302973107!3d10.84423499312376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175210002803ae7%3A0x8ed795132d2e7501!2zTmfDtSBH4buRbQ!5e0!3m2!1sen!2s!4v1718030733112!5m2!1sen!2s"
